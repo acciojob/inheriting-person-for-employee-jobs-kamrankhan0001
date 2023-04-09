56e6b2bd5d1f8c6bbfaac5554d04ce1ad6dc5a4e
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-09 10:39:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	greet(){
+		console.log('${this._name} my name is');
+		console.log('${this._name} I am years old');
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	jobGreet(){
+		console.log('${this._name} my name is');
+		console.log('${this._name} my job title is');
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
