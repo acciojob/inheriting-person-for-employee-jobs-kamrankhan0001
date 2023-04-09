752beca40b509aacef0e9eb2636b2fc9093af000
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-09 22:54:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,26 +6,19 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function() {
-  console.log(`'Hello, my name is Alice and I am 25 years old.'`);
+  console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
   Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
-
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function() {
-  console.log(`'Hello, my name is Alice and I am 25 years old.', and my job title is engineer.`);
-};
-const person = new Person("Alice", 25);
-person.greet();  // Output: Hello, my name is John, I am 30 years old.
-
-const employee = new Employee("Bob", 30, "Manager");
-employee.greet();   // Output: Hello, my name is Jane, I am 25 years old.
-employee.jobGreet(); // Output: Hello, my name is Jane, I am 25 years old, and my job title is Manager.
+  console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
+}
 
 
 // Do not change code below this line
