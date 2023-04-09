db7a6598a52dc829f771e76ccd24c1753c197e05
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-09 12:03:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // complete this js code
+
 function Person(name, age) {
   this.name = name;
   this.age = age;
@@ -19,10 +20,10 @@ Employee.prototype.constructor = Employee;
 Employee.prototype.jobGreet = function() {
   console.log(`'Hello, my name is Alice and I am 25 years old.', and my job title is engineer.`);
 };
-const person = new Person("John", 30);
+const person = new Person("Alice", 25);
 person.greet();  // Output: Hello, my name is John, I am 30 years old.
 
-const employee = new Employee("Jane", 25, "Manager");
+const employee = new Employee("Bob", 30, "Manager");
 employee.greet();   // Output: Hello, my name is Jane, I am 25 years old.
 employee.jobGreet(); // Output: Hello, my name is Jane, I am 25 years old, and my job title is Manager.
 
